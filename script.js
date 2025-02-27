f6d38451f8d87ceee7ff2013359393b660de0dbd
Bruninxntj <106992499+Bruninxntj@users.noreply.github.com>
2025-02-27 13:45:19 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+function finishGame() {
+    // Exibe a tela final com a mensagem de mistério
+    document.getElementById("puzzle-section").style.display = "none";
+    document.getElementById("final-screen").style.display = "block";
+}
